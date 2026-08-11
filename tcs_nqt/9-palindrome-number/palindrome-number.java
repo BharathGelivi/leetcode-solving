5362f0b0d0c7436075a1697ed2069906005e50b2
Bharath gelivi <155009564+BharathGelivi@users.noreply.github.com>
2026-08-11 22:28:45 +0530

    Time: 5 ms (81.23%) | Memory: 46.1 MB (16.61%) - LeetSync

--- tcs_nqt/9-palindrome-number/palindrome-number.java
+++ tcs_nqt/9-palindrome-number/palindrome-number.java
@@ -1,18 +1,15 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        // Negative numbers and numbers ending in 0 (except 0 itself) are not palindromes
-        if (x < 0 || (x % 10 == 0 && x != 0)) {
-            return false;
+        if (x < 0) return false;
+
+        long reversed = 0;
+        int temp = x;
+
+        while (temp != 0) {
+            reversed = reversed * 10 + temp % 10;
+            temp /= 10;
         }
 
-        int reversedHalf = 0;
-        while (x > reversedHalf) {
-            reversedHalf = reversedHalf * 10 + x % 10;
-            x /= 10;
-        }
-
-        // For even length numbers: x == reversedHalf (e.g., 1221 -> x = 12, reversedHalf = 12)
-        // For odd length numbers: x == reversedHalf / 10 (e.g., 121 -> x = 1, reversedHalf = 12)
-        return x == reversedHalf || x == reversedHalf / 10;
+        return reversed == x;
     }
 }
