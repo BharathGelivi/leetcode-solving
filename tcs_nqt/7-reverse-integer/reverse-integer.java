d007e858e5cf7b92317dff2d2ff8f8c79be6c3a4
Bharath gelivi <155009564+BharathGelivi@users.noreply.github.com>
2026-08-11 22:16:57 +0530

    Time: 1 ms (99.98%) | Memory: 42.5 MB (80.60%) - LeetSync

--- tcs_nqt/7-reverse-integer/reverse-integer.java
+++ tcs_nqt/7-reverse-integer/reverse-integer.java
@@ -6,11 +6,12 @@ class Solution {
             int rem = x % 10;
             x /= 10;
             
-            // Check overflow for positive numbers
+            // 1. Check positive overflow BEFORE multiplying rev by 10
             if (rev > Integer.MAX_VALUE / 10 || (rev == Integer.MAX_VALUE / 10 && rem > 7)) {
                 return 0;
             }
-            // Check overflow for negative numbers
+            
+            // 2. Check negative overflow BEFORE multiplying rev by 10
             if (rev < Integer.MIN_VALUE / 10 || (rev == Integer.MIN_VALUE / 10 && rem < -8)) {
                 return 0;
             }
